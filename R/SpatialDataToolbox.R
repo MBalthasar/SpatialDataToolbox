495b61146cf739b02f144b0a06e297b53602ae2f
MBalthasar <maphi@maxi-dsl.de>
2019-07-09 21:27:45 +0200

    Adjusted description.

--- R/SpatialDataToolbox.R
+++ R/SpatialDataToolbox.R
@@ -4,6 +4,14 @@
 #' This includes dissolving shapefiles based on feature attributes, converting the projection
 #' of raster and shapefiles to UTM and subsetting data into smaller tiles.
 #'
+#' @details Functions included in this package are:
+#' \itemize{
+#'   \item \code{\link{UTMConversion}}
+#'   \item \code{\link{SHPDissolve}}
+#'   \item \code{\link{FishnetFunction}}
+#'   \item \code{\link{TilesMaker}}
+#' }
+#'
 #' @docType package
 #'
 #' @author Marius Philipp \email{marius.philipp@stud-mail.uni-wuerzburg.de}
